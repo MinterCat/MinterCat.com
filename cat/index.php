433de43e03a894ac80ab3c699fe1186822aab80a
MinterCat <53012259+MinterCat@users.noreply.github.com>
2020-02-14 14:21:00 +0500

    Add files via upload

--- cat/index.php
+++ cat/index.php
@@ -3,6 +3,7 @@
 include('../../config/config.php');
 include('../function.php');
 session_start();
+$session_language = $_SESSION['session_language'];
 $cript_mnemonic = $_SESSION['cript_mnemonic'];
 $decript_text = openssl_decrypt($cript_mnemonic, $crypt_method, $crypt_key, $crypt_options, $crypt_iv);
 $decript = json_decode($decript_text,true);
@@ -15,7 +16,12 @@ $result = $db_users->query('SELECT * FROM "table" WHERE address="'.$address.'"')
 $data = $result->fetchArray(1);
 $check_language = $data['language'];
 
-if ($check_language != '') {$lang = $check_language;} else {$lang = 'English';}
+if ($check_language != '') 
+	{$lang = $check_language;} 
+else 
+	{
+		if ($session_language != '') {$lang = $session_language;} else {$lang = 'English';}
+	}
 
 $jsonlanguage = file_get_contents("https://raw.githubusercontent.com/MinterCat/Language/master/MinterCat_$lang.json");
 $language = json_decode($jsonlanguage,true);
