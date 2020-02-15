0106f71084458b823316534acd6fe701fd7a42ac
MinterCat <53012259+MinterCat@users.noreply.github.com>
2020-02-15 22:32:41 +0500

    Add files via upload

--- cat/index.php
+++ cat/index.php
@@ -10,6 +10,8 @@ include('../function.php');
 session_start();
 $session_language = $_SESSION['session_language'];
 $cript_mnemonic = $_SESSION['cript_mnemonic'];
+
+if ($cript_mnemonic != '') {
 $decript_text = openssl_decrypt($cript_mnemonic, $crypt_method, $crypt_key, $crypt_options, $crypt_iv);
 $decript = json_decode($decript_text,true);
 
@@ -20,7 +22,8 @@ $db_users = new Users();
 $result = $db_users->query('SELECT * FROM "table" WHERE address="'.$address.'"');
 $data = $result->fetchArray(1);
 $check_language = $data['language'];
-
+$login = $data['id'];
+}
 if ($check_language != '') 
 	{$lang = $check_language;} 
 else 
@@ -31,7 +34,7 @@ else
 $jsonlanguage = file_get_contents("https://raw.githubusercontent.com/MinterCat/Language/master/MinterCat_$lang.json");
 $language = json_decode($jsonlanguage,true);
 //========================================
-if ($address != '')
+if ($login != '')
 {
 function getBlockByHash ($api,$hash)
 {
