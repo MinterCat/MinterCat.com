bfd1573606c1dd248832c410bc7a91e7ccb4dbaa
MinterCat <53012259+MinterCat@users.noreply.github.com>
2020-03-06 21:58:47 +0500

    Add files via upload

--- api/gen/full/index.php
+++ api/gen/full/index.php
@@ -6,12 +6,15 @@ class Cats extends SQLite3
 {
     function __construct()
     {
-        $this->open('../../../config/cats.sqlite');
+        $this->open('../../../../config/cats.sqlite');
     }
 }
 
 $db_cats = new Cats();
-$result = $db_cats->query('SELECT * FROM "gen"');
+$result = $db_cats->query('
+SELECT "table".*, "gen".*
+FROM "table" INNER JOIN "gen"
+ON "table".stored_id = "gen".stored_id;');
 $data = array();
 while ($res = $result->fetchArray(1)){array_push($data, $res);}
 echo json_encode($data);
