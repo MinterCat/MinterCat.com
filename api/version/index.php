ace748efbaeb166261ab8856e410a8e9fc64425b
MinterCat <53012259+MinterCat@users.noreply.github.com>
2020-03-12 23:27:48 +0500

    API update

--- api/version/index.php
+++ api/version/index.php
@@ -4,10 +4,11 @@ header('Content-type: application/json; charset=utf-8');
 
 $array = array(
 "php" => "v7.1.3",
-"master" => "v0.1.0-Beta2",
+"github.com/MinterCat/MinterCat.com" => "v0.1.0-Beta2",
 "api" => "v0.1.0",
-"blockchain" => "Minter v1.1.3",
-"minter-php-sdk" => "v2.2.0",
+"blockchain" => "Minter",
+"github.com/MinterTeam/minter-go-node" => "v1.1.3",
+"github.com/MinterTeam/minter-php-sdk" => "v2.2.0",
 );
 
 echo json_encode($array, JSON_UNESCAPED_UNICODE);
