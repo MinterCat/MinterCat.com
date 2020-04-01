8eb6398d30a53da283263bbdb0f0ebc1ae7bd550
MinterCat <admin@mintercat.com>
2020-04-02 01:18:26 +0500

    MinterCat | Refund

---
+++ refund/index.php
@@ -0,0 +1,22 @@
+<?php
+//========================================
+echo "<title>MinterCat | Refund</title>";
+$titles = 'Refund';
+$m = 3;
+//-------------------------------
+include('../header2.php');
+//-------------------------------
+echo '
+<div class="cat_content" style="text-align: left; float: none;">
+<ul>
+<li><a href="zen" target="_blank">Zen node</a></li><br>
+<li><a href="unode" target="_blank">U-node</a></li><br>
+<li><a href="bipmaker" target="_blank">BipMaker.PRO node</a></li><br>
+<li><a href="en" target="_blank">Enlightenment node</a></li><br>
+<li><a href="musk" target="_blank">Elon Musk node</a></li><br>
+</ul>
+</div>
+<br><br><br>
+';
+//-------------------------------
+include('../footer.php');
