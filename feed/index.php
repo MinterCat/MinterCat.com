37bbbdfa4e4b5abd0e20509be31eeadddbbd1680
MinterCat <admin@mintercat.com>
2020-04-03 04:44:34 +0500

    MinterCat | Feed

--- feed/index.php
+++ feed/index.php
@@ -6,12 +6,20 @@ $m = 0;
 //-------------------------------
 include('../header2.php');
 echo '<br>';
-function TG()
+
+function TGgetUpdates()
 {
 	include(explode('public_html', $_SERVER['DOCUMENT_ROOT'])[0] . 'config/config.php');
 	$data = file_get_contents('https://api.telegram.org/bot'.$token_feed_bot.'/getUpdates');
     $tg_array = json_decode($data)->result;
 	return array_reverse($tg_array);
+}
+function TGgetFile($photo)
+{
+	include(explode('public_html', $_SERVER['DOCUMENT_ROOT'])[0] . 'config/config.php');
+	$file_path = json_decode(file_get_contents('https://api.telegram.org/bot'.$token_feed_bot.'/getFile?file_id='. $photo))->result->file_path;
+	$data = 'https://api.telegram.org/file/bot'.$token_feed_bot.'/'. $file_path;
+	return $data;
 }
 
 echo '
@@ -19,10 +27,22 @@ echo '
 <div class="explorer_content">
 ';
 $i=1;
-foreach (TG() as $value => $test) {
+foreach (TGgetUpdates() as $value => $test) {
 	$value++;
+	$text = $test->channel_post->text;
+	
+	$photo = json_decode(json_encode($test->channel_post->photo),true);
+	if ($photo != '') {
+		$caption = $test->channel_post->caption;
+		$file_id = $photo[0]['file_id'];
+		$data = TGgetFile($file_id);
+		$width = $photo[0]['width'];
+		$height = $photo[0]['height'];
+	}
+	
 	$id = $test->channel_post->chat->id;
-	if ($id == '-1001304129205')
+	//if ($id == '-1001304129205')
+	if ($id == '-1001482536175')
 	{
 		$i++;
 		echo '
@@ -31,7 +51,14 @@ foreach (TG() as $value => $test) {
 		<div class="explorer_block_content">
 		<br>
 		';
-		echo $text = $test->channel_post->text;
+		if ($photo != '') {
+			echo "<img src='".$data."' width='".$width."' height='".$height."'><br>";
+			echo $caption;		
+			}
+			else {
+				echo $text;
+			}
+		
 		echo '</div></div>';
 		if ($i > 5) {break;}
 	}
