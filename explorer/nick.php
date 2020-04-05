fc98f32b49813d6c92e119b434c92ca464322830
MinterCat <admin@mintercat.com>
2020-04-06 00:15:32 +0500

    test MinterCat PHP SDK

--- explorer/nick.php
+++ explorer/nick.php
@@ -1,77 +1,54 @@
 <?php
-$data = $db_users->query('SELECT * FROM "table" WHERE nick="' . $nick . '"')->fetchArray(1);
-$address = $data['address'];
-$get = file_get_contents($site."api/cats?addr=$address");
-$payloads1 = json_decode($get,true);
-$result = (count($payloads1)-1);
+$Language = Language('Russian');
+
+$address = User::Nick($nick)->address;
+$array_cats = Cats::Address($address);
+
+$result = (count($array_cats)-1);
 $countq = ceil(($result+1)/12);
+
 echo "<center><h2><b>$nick</b></h2></center><hr>";
-
-$json4 = file_get_contents($site.'api');
-$payloads4 = json_decode($json4,true);
-$cats = $payloads4['cats'];
-$ccount = (count($cats))-1;
-
-if ($countq != 0)
-{
-foreach ($payloads1 as $value => $kity) {
+foreach ($array_cats as $value => $kity) {
 	$value++;
 	if ($value == 1) {
 	$id = 1;
 	echo '<div class="cat_content_none"><div class="cat_content" id="page-'.$id.'">';
 	}
-	//-------------------------------------------------
 
-	$pricebd = $kity['price'];
-	$img = $kity['img'];
-	$block = $kity['stored_id'];
-	for ($y = 0; $y<=$ccount; $y++)
-		{
-			$catimg = $cats[$y]['img'];
-			if ($catimg == $img)
-				{
-					$series = $cats[$y]['series'];
-					$rarity = ($cats[$y]['rarity'])*100;
-					$price = $cats[$y]['value'];
-					$name1 = $cats[$y]['name'];
-					$count = $cats[$y]['count'];
-					$gender = $cats[$y]['gender'];
-				}
-		}
-	$name2 = $kity['name'];
+	$img = $kity->img;
+	$cat = Cats::Img($img);
+	
+	$series = $cat->series;
+	$rarity = ($cat->rarity)*100;
+	$price = $cat->value;
+	$name1 = $cat->name;
+	$count = $cat->count;
+	$gender = $cat->gender;
+	$color = $cat->color;
+	
+	$name2 = $kity->name;
 	if (($name2 != '') and ($name2 != null)) {$name = $name2;} else {$name = $name1;}
-	if ($pricebd == '') {$bgimg = ''; $prr = $price;} else {$bgimg = '<font color="red"><b>(Продается)</b></font>'; $prr = "<font color='red'><b>$pricebd</b></font>";}
+	if ($pricebd == '') {$bgimg = ''; $prr = $price;} else {$bgimg = '<font color="red"><b>(Sale)</b></font>'; $prr = "<font color='red'><b>$pricebd</b></font>";}
 		if ($gender == '0') {$gender = '';}
-
-		switch ($series)
-		{
-			case 0: {$u = '#C1B5FF'; break;}
-			case 1: {$u = '#FFF6B5'; break;}
-			case 2: {$u = '#FFB5B5'; break;}
-			case 3: {$u = '#C7F66F'; break;}
-			case 4: {$u = '#FFC873'; break;}
-			case 5: {$u = '#6AF2D7'; break;}
-			case 999: {$u = '#9BF5DA'; break;}
-		}
-		if ($img != '') {
-				echo "
-					<div class='cat_block' style='background: $u'>
-						<div class='cat_img'>
-							<a href='".$site."cat?id=$block'>
-								<picture>
-									<source srcset='../static/img/Cat$img.webp' type='image/webp'>
-									<img src='../png.php?png=$img'>
-								</picture>
-							</a>
-						</div>
-						<div class='cat_text'>
-							#$block $bgimg<br>
-							<hr>
-							$name $gender<br>
-							$prr $coin<br>
-						</div>
-					</div>";
-}//-------------------------------------------------
+	
+	echo "
+	<div class='cat_block' style='background: $color'>
+		<div class='cat_img'>
+			<picture>
+			<source srcset='https://mintercat.com/static/img/Cat$img.webp' type='image/webp'>
+			<img src='https://mintercat.com/png.php?png=$img'>
+			</picture>
+		</div>
+		<div class='cat_text'>
+			<b>$name</b> $gender
+			<hr>
+			$Language->Number_of_cats_of_this_breed <b>$count</b><br>
+			$Language->Chance_of_falling_out : <b>$rarity%</b><br>
+			<b>$price</b> MINTERCAT
+			<br>
+		</div>
+	</div>";
+//-------------------------------------------------
 if ($value % 12 == 0) {
 	echo "</div></div>";
 	$id++;
@@ -79,7 +56,6 @@ if ($value % 12 == 0) {
 	}
 }
 echo "</div></div>";
-}
 echo "<br><div class='cat_form'>
 			<div class='pagination'>
 				<button type='button' id='prev-page-btn' disabled>«</button>
